02e191c0e594dc4c64f2ed21fb746bd68a5c2326
Luis Aranda <56684878+Lerne70@users.noreply.github.com>
2025-04-12 20:02:26 -0600

    Fin seccion 7

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  //Esto se coloca antes del nombre de los controladores
+  // ejemplo 'http://localhost:3000/api/v2/pokemon'
+  app.setGlobalPrefix('api/v2');
+
+  app.useGlobalPipes(  
+    new ValidationPipe({ 
+      whitelist: true, 
+      forbidNonWhitelisted: true, 
+    }) 
+  );
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
